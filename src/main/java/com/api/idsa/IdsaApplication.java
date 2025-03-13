87c5de49159514bd8fd84e9449bc36bc49d10041
Caballero-dev <153244973+Caballero-dev@users.noreply.github.com>
2025-03-12 21:03:49 -0600

    first commit

---
+++ src/main/java/com/api/idsa/IdsaApplication.java
@@ -0,0 +1,13 @@
+package com.api.idsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdsaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/idsa/IdsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.idsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
