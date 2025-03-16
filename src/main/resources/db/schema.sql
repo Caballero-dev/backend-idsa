dec2bc455211ca938d32345720d2b57d3dbb1bad
Caballero-dev <153244973+Caballero-dev@users.noreply.github.com>
2025-03-16 16:56:59 -0600

    Add initial database schema

---
+++ src/main/resources/db/schema.sql
@@ -0,0 +1,135 @@
+CREATE DATABASE idsa;
+
+CREATE TABLE campuses
+(
+    campus_id   SERIAL,
+    campus_name VARCHAR(100) UNIQUE NOT NULL,
+    PRIMARY KEY (campus_id)
+);
+
+CREATE TABLE modalities
+(
+    modality_id   SERIAL,
+    modality_name VARCHAR(50) UNIQUE NOT NULL,
+    PRIMARY KEY (modality_id)
+);
+
+CREATE TABLE especialities
+(
+    especiality_id   SERIAL,
+    especiality_name VARCHAR(100) UNIQUE NOT NULL,
+    short_name       VARCHAR(10) UNIQUE  NOT NULL,
+    PRIMARY KEY (especiality_id)
+);
+
+CREATE TABLE grades
+(
+    grade_id   SERIAL,
+    grade_name VARCHAR(30) UNIQUE NOT NULL,
+    PRIMARY KEY (grade_id)
+);
+
+CREATE TABLE groups
+(
+    group_id   SERIAL,
+    group_name VARCHAR(2) UNIQUE NOT NULL,
+    PRIMARY KEY (group_id)
+);
+
+CREATE TABLE generations
+(
+    generation_id SERIAL,
+    start_year    DATE NOT NULL,
+    end_year      DATE NOT NULL,
+    PRIMARY KEY (generation_id),
+    CONSTRAINT unique_generation UNIQUE (start_year, end_year)
+);
+
+CREATE TABLE roles
+(
+    role_id   SERIAL,
+    role_name VARCHAR(50) UNIQUE NOT NULL,
+    PRIMARY KEY (role_id)
+);
+
+CREATE TABLE users
+(
+    user_id    SERIAL,
+    role_id    INT                 NOT NULL,
+    email      VARCHAR(100) UNIQUE NOT NULL,
+    password   VARCHAR(255)        NOT NULL,
+    created_at TIMESTAMP           NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    is_active  BOOLEAN             NOT NULL DEFAULT TRUE,
+    PRIMARY KEY (user_id),
+    FOREIGN KEY (role_id) REFERENCES roles (role_id)
+);
+
+CREATE TABLE people
+(
+    person_id      SERIAL,
+    user_id        INT                NOT NULL,
+    name           VARCHAR(50)        NOT NULL,
+    first_surname  VARCHAR(50)        NOT NULL,
+    second_surname VARCHAR(50)        NOT NULL,
+    phone_number   VARCHAR(10) UNIQUE NOT NULL,
+    PRIMARY KEY (person_id),
+    FOREIGN KEY (user_id) REFERENCES users (user_id)
+);
+
+CREATE TABLE tutors
+(
+    tutor_id      SERIAL,
+    person_id     INT                NOT NULL,
+    employee_code VARCHAR(20) UNIQUE NOT NULL,
+    PRIMARY KEY (tutor_id),
+    FOREIGN KEY (person_id) REFERENCES people (person_id)
+);
+
+CREATE TABLE group_configuration
+(
+    group_configuration_id SERIAL,
+    campus_id              INT NOT NULL,
+    especiality_id         INT NOT NULL,
+    modality_id            INT NOT NULL,
+    grade_id               INT NOT NULL,
+    group_id               INT NOT NULL,
+    generation_id          INT NOT NULL,
+    tutor_id               INT NOT NULL,
+    PRIMARY KEY (group_configuration_id),
+    CONSTRAINT unique_group_configuration UNIQUE (campus_id, especiality_id, modality_id, grade_id, group_id,
+                                                  generation_id),
+    FOREIGN KEY (campus_id) REFERENCES campuses (campus_id),
+    FOREIGN KEY (modality_id) REFERENCES modalities (modality_id),
+    FOREIGN KEY (especiality_id) REFERENCES especialities (especiality_id),
+    FOREIGN KEY (grade_id) REFERENCES grades (grade_id),
+    FOREIGN KEY (group_id) REFERENCES groups (group_id),
+    FOREIGN KEY (generation_id) REFERENCES generations (generation_id),
+    FOREIGN KEY (tutor_id) REFERENCES tutors (tutor_id)
+);
+
+CREATE TABLE students
+(
+    student_id             SERIAL,
+    person_id              INT                NOT NULL,
+    group_configuration_id INT                NOT NULL,
+    student_code           VARCHAR(20) UNIQUE NOT NULL,
+    PRIMARY KEY (student_id),
+    FOREIGN KEY (group_configuration_id) REFERENCES group_configuration (group_configuration_id),
+    FOREIGN KEY (person_id) REFERENCES people (person_id)
+);
+
+CREATE TABLE reports
+(
+    report_id            SERIAL,
+    student_id           INT           NOT NULL,
+    images_paths         VARCHAR[],
+    temperature          NUMERIC(4, 2) NOT NULL,
+    pupil_dilation_right NUMERIC(4, 2) NOT NULL,
+    pupil_dilation_left  NUMERIC(4, 2) NOT NULL,
+    heart_rate           NUMERIC(4, 2) NOT NULL,
+    oxygen_levels        NUMERIC(4, 2) NOT NULL,
+    prediction_result    NUMERIC(5, 2) NOT NULL,
+    created_at           TIMESTAMP     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (report_id),
+    FOREIGN KEY (student_id) REFERENCES students (student_id)
+);
