fdca2c489f2217ff3e3de7ed9c20b8b97d6e8323
Caballero-dev <153244973+Caballero-dev@users.noreply.github.com>
2025-03-17 14:43:44 -0600

    Add validation annotations to CampusRequest and update CampusController methods

--- src/main/java/com/api/idsa/dto/request/CampusRequest.java
+++ src/main/java/com/api/idsa/dto/request/CampusRequest.java
@@ -1,5 +1,9 @@
 package com.api.idsa.dto.request;
 
+import com.api.idsa.util.RegexPatterns;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,6 +13,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class CampusRequest {
 
+    @NotBlank(message = "Name cannot be blank")
+    @Size(min = 3, max = 100, message = "Name must be between 3 and 100 characters")
+    @Pattern(regexp = RegexPatterns.ONLY_LETTERS, message = "Name can only contain letters and spaces")
     private String name;
 
 }
