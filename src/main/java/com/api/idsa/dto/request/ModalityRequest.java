eae67bc83e05a120b93f499b04b988e08e383bde
Caballero-dev <153244973+Caballero-dev@users.noreply.github.com>
2025-03-17 14:10:52 -0600

    Implement modality management with CRUD operations and validation

---
+++ src/main/java/com/api/idsa/dto/request/ModalityRequest.java
@@ -0,0 +1,18 @@
+package com.api.idsa.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ModalityRequest {
+
+    @NotBlank(message = "Name cannot be blank")
+    @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters")
+    private String name;
+
+}
