27fc82f812db2749648951cac239eaedc369a3b7
Caballero-dev <153244973+Caballero-dev@users.noreply.github.com>
2025-06-18 11:35:25 -0600

    Refactor: Improve exception handling and error messages in ModalityServiceImpl
    - Add ResourceDependencyException import for better resource management.
    - Update DuplicateResourceException and ResourceNotFoundException messages for clarity.
    - Enhance deleteModality method to handle DataIntegrityViolationException with specific messages for resource dependencies.

--- src/main/java/com/api/idsa/domain/academic/service/impl/ModalityServiceImpl.java
+++ src/main/java/com/api/idsa/domain/academic/service/impl/ModalityServiceImpl.java
@@ -1,6 +1,7 @@
 package com.api.idsa.domain.academic.service.impl;
 
 import com.api.idsa.common.exception.DuplicateResourceException;
+import com.api.idsa.common.exception.ResourceDependencyException;
 import com.api.idsa.common.exception.ResourceNotFoundException;
 import com.api.idsa.domain.academic.dto.request.ModalityRequest;
 import com.api.idsa.domain.academic.dto.response.ModalityResponse;
@@ -13,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.dao.DataIntegrityViolationException;
 
 @Service
 public class ModalityServiceImpl implements IModalityService {
@@ -33,7 +35,7 @@ public class ModalityServiceImpl implements IModalityService {
     public ModalityResponse createModality(ModalityRequest modalityRequest) {
 
         if (modalityRepository.existsByModalityName(modalityRequest.getName())) {
-            throw new DuplicateResourceException("create", "Modality", modalityRequest.getName());
+            throw new DuplicateResourceException("Modality", "name", modalityRequest.getName());
         }
 
         ModalityEntity modalityEntity = modalityMapper.toEntity(modalityRequest);
@@ -44,10 +46,10 @@ public class ModalityServiceImpl implements IModalityService {
     public ModalityResponse updateModality(Long modalityId, ModalityRequest modalityRequest) {
         
         ModalityEntity modalityEntity = modalityRepository.findById(modalityId)
-                .orElseThrow(() -> new ResourceNotFoundException("update", "Modality", modalityId));
+                .orElseThrow(() -> new ResourceNotFoundException("Modality", "id", modalityId));
         
         if (!modalityEntity.getModalityName().equals(modalityRequest.getName()) && modalityRepository.existsByModalityName(modalityRequest.getName())) {
-            throw new DuplicateResourceException("update", "Modality", modalityRequest.getName());
+            throw new DuplicateResourceException("Modality", "name", modalityRequest.getName());
         }
 
         modalityEntity.setModalityName(modalityRequest.getName());
@@ -56,11 +58,17 @@ public class ModalityServiceImpl implements IModalityService {
     
     @Override
     public void deleteModality(Long modalityId) {
-
-        ModalityEntity modalityEntity = modalityRepository.findById(modalityId)
-                .orElseThrow(() -> new ResourceNotFoundException("delete", "Modality", modalityId));
-        
-        modalityRepository.delete(modalityEntity);
+        try {
+            ModalityEntity modalityEntity = modalityRepository.findById(modalityId)
+                    .orElseThrow(() -> new ResourceNotFoundException("Modality", "id", modalityId));
+            modalityRepository.delete(modalityEntity);
+        } catch (DataIntegrityViolationException e) {
+            if (e.getMessage().contains("group_configurations_modality_id_fkey")) {
+                throw new ResourceDependencyException("Modality", modalityId, "assigned group configurations", "group_configurations");
+            } else {
+                throw new ResourceDependencyException("Modality", modalityId, "associated records", "unknown");
+            }
+        }
     }
     
 }
